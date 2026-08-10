b52e16b36e4d86207546dba9d0b41b7bfcb4d829
Soumya9107 <soumyjitmaity31@gmail.com>
2026-08-10 07:10:24 +0530

    added models

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,10 +1,17 @@
 //const express = require('express');
 import express from 'express';
 import "dotenv/config";
+import User from "./models/user_model.js";
+import { connectDB } from "./lib/db.js";
 
 const app = express();
-const PORT = process.env.PORT
+const PORT = process.env.PORT;
+
+app.get("health", (req, res) => {
+    res.status(200).json({ message: "Server is healthy" });
+});
 
 app.listen(PORT, () => {
+    connectDB();
     console.log('Server is running on PORT:', PORT);
 });
