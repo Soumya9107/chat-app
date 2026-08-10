0fbf0667cc7af484d8075ac36050727562a18dc8
Soumya9107 <soumyjitmaity31@gmail.com>
2026-08-10 23:37:52 +0530

    added database connection and cors

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,13 +1,20 @@
 //const express = require('express');
-import express from 'express';
+import express from "express";
+import cors from "cors";
 import "dotenv/config";
+import { clerkMiddleware } from "@clerk/express";
 import User from "./models/user_model.js";
 import { connectDB } from "./lib/db.js";
 
 const app = express();
 const PORT = process.env.PORT;
+const FRONTEND_URL = process.env.FRONTEND_URL;
 
-app.get("health", (req, res) => {
+app.use(express.json());
+app.use(cors({ origin: FRONTEND_URL, credentials: true}));
+app.use(clerkMiddleware());
+
+app.get("/health", (req, res) => {
     res.status(200).json({ message: "Server is healthy" });
 });
 
