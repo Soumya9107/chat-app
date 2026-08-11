d9e49e7a7d48d8367025465eb9c534b75a4cfddb
Soumya9107 <soumyjitmaity31@gmail.com>
2026-08-11 23:38:38 +0530

    added Dockerfile and .dockerignore

--- backend/src/index.js
+++ backend/src/index.js
@@ -2,13 +2,20 @@
 import express from "express";
 import cors from "cors";
 import "dotenv/config";
+
+import fs from "fs";
+import path from "path";
+
 import { clerkMiddleware } from "@clerk/express";
 import User from "./models/user_model.js";
 import { connectDB } from "./lib/db.js";
 
 const app = express();
+
 const PORT = process.env.PORT;
 const FRONTEND_URL = process.env.FRONTEND_URL;
+
+const publicDir = path.join(process.cwd(), "public");
 
 app.use(express.json());
 app.use(cors({ origin: FRONTEND_URL, credentials: true}));
@@ -18,6 +25,13 @@ app.get("/health", (req, res) => {
     res.status(200).json({ message: "Server is healthy" });
 });
 
+if (fs.existsSync(publicDir)) {
+    app.use(express.static(publicDir));
+
+    app.get("/{*any}", (req, res, next) => {
+        res.sendFile(path.join(publicDir, "index.html"), (err) => next(err));
+    });
+}
 app.listen(PORT, () => {
     connectDB();
     console.log('Server is running on PORT:', PORT);
