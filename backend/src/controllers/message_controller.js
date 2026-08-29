18dbb3817d0a9aa4873aeb98e864a47d8d0913ff
Soumya9107 <soumyjitmaity31@gmail.com>
2026-08-29 23:58:04 +0530

    feat: fix import statement and enhance conversation retrieval logic

--- backend/src/controllers/message_controller.js
+++ backend/src/controllers/message_controller.js
@@ -1,4 +1,5 @@
 import User from "../models/user_model.js";
+import User from "../models/message_model.js";
 
 export async function getUsersForSidebar(req, res) {
     try {
@@ -13,8 +14,23 @@ export async function getUsersForSidebar(req, res) {
 
 export async function getConversationsForSidebar(req, res) { 
     try {
+        const loggedInUserId = req.user._id;
 
-    } catch (error) { 
-        
+        const conversations = await Message.aggregate([
+            { $match: { $or: [{ senderId: loggedInUserId }, { receiverId: loggedInUserId }] } },
+
+            {
+                $group: {
+                    _id: {
+                        $cond: [
+                            { $eq: ["$senderId", loggedInUserId] }, "$receiverId", "$senderId"]
+                    },
+                    lastMessageAt: { $max: "$createdAt" },
+            },
+        ]);
+        res.status(200).json(conversations);
+    } catch (error) {
+        console.error("Error in getConversationsForSidebar:", error.message);
+        res.status(500).json({ message: "Internal server error" });
     }
 }
