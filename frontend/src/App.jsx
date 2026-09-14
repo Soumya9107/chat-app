9989b5ebde211c879f12251c185bd99580233bf8
Soumya9107 <soumyjitmaity31@gmail.com>
2026-09-14 23:29:19 +0530

    feat: add routing and theme/wallpaper context providers; implement Auth and Chat pages

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,22 +1,27 @@
-import { Show, SignInButton, SignUpButton, UserButton } from "@clerk/react";
-import { Button } from '@heroui/react';
+import { ThemeProvider } from './context/ThemeContext';
+import { WallpaperProvider } from './context/WallpaperContext';
+import { Route, Routes } from 'react-router';
+import ChatPage from './pages/ChatPage';
+import AuthPage from './pages/AuthPage';
+import { useAuth } from '@clerk/react';
 
 function App() {
-  return (
-    <div>
-      <h1 className="text-4xl text-red-500 bg-blue-400">MY APP</h1>
-      <Button>My Button</Button>
 
-      <header>
-        <Show when="signed-out">
-          <SignInButton mode="modal" />
-          <SignUpButton mode="modal" />
-        </Show>
-        <Show when="signed-in">
-          <UserButton />
-        </Show>
-      </header>
-    </div>
+  const { isSignedIn, isLoaded } = useAuth();
+
+  if (!isLoaded) {
+    return <p>Loading...</p>;
+  }
+  return (
+    <ThemeProvider>
+      <WallpaperProvider>
+        <Router>
+          <Route path="/" element={isSignedIn ? <ChatPage /> : <Navigate to={"/auth"} replace />} />
+          <Route path="/auth" element={!isSignedIn ? <AuthPage /> : <Navigate to={"/chat"} replace />} />
+
+        </Router>
+      </WallpaperProvider>
+    </ThemeProvider>
   );
 }
 
